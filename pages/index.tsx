752ecf96bc856d4b48df3dc20118eb70926ab458
tvergho <tvergho@gmail.com>
2021-03-01 21:31:31 -0800

    Basic page background styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Layout from 'components/Layout';
+import styles from 'styles/Home.module.scss';
 
 const Home = (): JSX.Element => {
   return (
-    <Layout title="Home" description="TeamThink is a group interview platform designed to make testing teams easy." />
+    <Layout title="Home" description="TeamThink is a group interview platform designed to make testing teams easy." className={styles.home} />
   );
 };
 
